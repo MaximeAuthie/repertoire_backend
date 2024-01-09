b1f19975df3c0bde1ca5679e422884cf95862510
Maxime Authié <authie.maxime@orange.fr>
2024-01-09 15:06:41 +0100

    body-parser + env variables + helper

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-//! Initialiser les variables d'accès au serveur
-const hostname = 'http://127.0.0.1';
-const port = 3000;
-
-//! Importer le routeurs
+//! Importer les routeurs
 const contactRouter = require('./routes/contact.routes');
 const categoryRouter = require('./routes/category.routes');
 const userRouter = require('./routes/user.routes');
@@ -10,19 +6,35 @@ const userRouter = require('./routes/user.routes');
 //! Importer Express
 const express = require('express');
 
-//! Importer Mongoose
-const mongoose = require('mongoose');
+//! Importer Dotenv pour gérer les variables d'environnement
+require('dotenv').config();
 
 //! Créer l'app
 const app = express();
 
+//! Importer Mongoose
+const mongoose = require('mongoose');
+
+//! Importer et paramétrer le package body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 //! Connecter la BDD
-mongoose.connect('mongodb://localhost/repertoire');
+mongoose.connect(process.env.DB_URL);
 
 //! Gérer les routes
-// app.use('/contacts', contactRouter);
-// app.use('/categories', categoryRouter);
-// app.use('/user', userRouter);
+app.use('/contacts', contactRouter);
+app.use('/categories', categoryRouter);
+app.use('/users', userRouter);
+
+//! Ecouter les erreurs du helper catchAsync
+app.use((err, req, res, next) => {
+    console.log(err);
+    res
+        .status(500)
+        .send(err);
+})
 
 //! Ecouter le port
-app.listen(3000, () => console.log(`Application lancée sur ${hostname}:${port}`));
+app.listen(3000, () => console.log(`Application lancée sur ${process.env.HOSTNAME}:${process.env.PORT}`));
