bff558ef28cee13bf3d46f57f7ca1ae30d87feb2
Maxime Authié <authie.maxime@orange.fr>
2024-01-09 18:08:43 +0100

    Ajout helper catchAsync

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,23 @@
 //! Importer le modèle "user"
 const User = require('../models/user');
 
+//! Importer le helper "catchAsync"
+const catchAsync = require('../helpers/catchAsync');
+const { StatusCodes } = require('http-status-codes');
+
 //! Liste des méthodes
 
     //? Méthode pour ajouter un utilisateur
-    const addUser = async (req, res) => {
+    const addUser = catchAsync(async (req, res) => {
+
+        // Ajout de l'utilisateur en BDD
         const user = await User.create(req.body);
-        res.send(user);
-    }
+
+        // Si la requête s'est exécutée correctement
+        res
+            .status(StatusCodes.ACCEPTED)
+            .send(user);
+    });
 
 //! Exporter les méthodes
 module.exports = {
