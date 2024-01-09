9b5e321bc1d744f2ca77481a1bdb2c52b4fd009c
Maxime Authié <authie.maxime@orange.fr>
2024-01-09 18:08:21 +0100

    Ajout updateContact et deleteContact

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -7,28 +7,7 @@ const { StatusCodes } = require('http-status-codes');
 
 //! Liste des méthodes
 
-    //? Méthode pour obtenir la liste des contacts de l'utilisateur
-    const getAllContactsByUser = catchAsync(async (req, res) => {
-        
-        // Récupérer les contact grace à l'id de l'utilisateur
-        const contacts = await Contact.find({
-            user: req.params.id
-        });
-
-        // Si la requête ne renvoie pas de résultats
-        if (!contacts.length) {
-            res
-                .status(StatusCodes.NO_CONTENT)
-                .send('No contact for this user')
-        }
-        
-        // Si la requête renvoie des résultats
-        res
-            .status(StatusCodes.ACCEPTED)
-            .send(contacts);
-    });
-
-    //? Méthode pour ajouter un contact
+    //? Ajouter un contact
     const addContact = catchAsync(async (req, res) => {
 
         // Ajouter le contact à la BDD
@@ -40,9 +19,86 @@ const { StatusCodes } = require('http-status-codes');
             .send(contact);
     });
 
+    //? Obtenir un contact à partir de son id
+    const getContactById = catchAsync(async (req, res) => {
+
+        // Récupérer le contact grace à son id
+        const contact = await Contact.findById(req.params.id);
+
+        // Vérifier si la requête renvoie un résultat
+        if (contact) {
+            res
+            .status(StatusCodes.ACCEPTED)
+            .send(contact);
+        } else {
+            res
+                .status(StatusCodes.NO_CONTENT)
+                .send('The requested contact doesn\'t exist');
+        }
+
+    })
+
+    //? Obtenir la liste des contacts de l'utilisateur
+    const getAllContactsByUser = catchAsync(async (req, res) => {
+        
+        // Récupérer les contact grace à l'id de l'utilisateur
+        const contacts = await Contact.find({
+            user: req.params.id
+        });
+
+        // Vérifier si la requête renvoie un résultat
+        if (contacts.length) {
+            res
+            .status(StatusCodes.ACCEPTED)
+            .send(contacts);
+        } else {
+            res
+                .status(StatusCodes.NO_CONTENT)
+                .send('No contact found for this user')
+        }
+    });
+
+    //? Modifier un contact à partir de son id
+    const updateContact = catchAsync(async (req, res) => {
+
+        // Mettre à jour le contact
+        const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+        // Vérifier si la requête renvoie un résultat
+        if (contact) {
+            res
+                .status(StatusCodes.ACCEPTED)
+                .send(contact);
+        } else {
+            res
+                .status(StatusCodes.NOT_FOUND)
+                .send('The requested contact doesn\'t exist');
+        }
+    })
+
+    //? Supprimer un contact à partir de son id
+    const deleteContact = catchAsync(async (req, res) => {
+
+        // Supprimer le contact
+        const contact = await Contact.findByIdAndDelete(req.params.id)
+
+        // Vérifier si la requête renvoie un résultat
+        if (contact) {
+            res
+                .status(StatusCodes.ACCEPTED)
+                .send('Contact deleted with success');
+        } else {
+            res
+                .status(StatusCodes.NOT_FOUND)
+                .send('The requested contact doesn\'t exist')
+        }
+    } )
 
 //! Exporter les méthodes
 module.exports = {
+    addContact,
+    getContactById,
     getAllContactsByUser,
-    addContact
+    updateContact,
+    deleteContact
 }
--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -9,11 +9,20 @@ const contactController = require('../controllers/contact.controller');
 
 //! Liste des routes
 
-    //? Obtenir tous les contacts
-    router.get('/list/:id', contactController.getAllContactsByUser);
-
     //? Ajouter un contact
     router.post('/add', contactController.addContact);
+    
+    //? Obtenir un contact par son id
+    router.get('/:id', contactController.getContactById);
+
+    //? Obtenir tous les contacts d'un utilisateur
+    router.get('/list/:id', contactController.getAllContactsByUser);
+    
+    //? Modifier un contact à partir de son id
+    router.patch('/update/:id', contactController.updateContact);
+
+    //? Supprimer un contact à partir de son id
+    router.delete('/delete/:id', contactController.deleteContact);
 
 //! Exporter le router
 module.exports = router;
