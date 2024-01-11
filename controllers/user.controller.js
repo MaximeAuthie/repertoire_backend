07f893ae7fd0c68532bd5f16f4e2e8e2276798b2
Maxime Authié <authie.maxime@orange.fr>
2024-01-11 17:54:52 +0100

    Hashage password

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,18 +5,40 @@ const User = require('../models/user');
 const catchAsync = require('../helpers/catchAsync');
 const { StatusCodes } = require('http-status-codes');
 
+//! Importer la dépendance Bcrypt
+const bcrypt = require('bcrypt');
+
 //! Liste des méthodes
 
     //? Méthode pour ajouter un utilisateur
     const addUser = catchAsync(async (req, res) => {
+        
+        // Vérifier si l'utilisateur existe déjà
+        if (!(await User.find({email: req.body.email})).length) {
+            
+            // Eclater le body
+            const {firstName, lastName, email, password} = req.body
 
-        // Ajout de l'utilisateur en BDD
-        const user = await User.create(req.body);
+            // Hasher le password
+            bcrypt.hash(password, parseInt(process.env.BCRYP_SALT_ROUND)).then(async hash => {
+                req.body.password = hash;
 
-        // Si la requête s'est exécutée correctement
-        res
-            .status(StatusCodes.ACCEPTED)
-            .send(user);
+                // Ajout de l'utilisateur en BDD
+                const user = await User.create(req.body);
+
+                // Si la requête s'est exécutée correctement
+                res
+                    .status(StatusCodes.ACCEPTED)
+                    .send(user);
+            });
+
+        } else {
+
+            // L'utilisateur existe déjà
+            res
+                .status(StatusCodes.CONFLICT)
+                .send('The email adress is already used')
+        }
     });
 
 //! Exporter les méthodes
