0b23cb9722108c6d143aea3c952d90a3688fbd0c
Maxime Authié <authie.maxime@orange.fr>
2024-01-18 16:49:03 +0100

    Modif config Netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,23 +1,38 @@
 //! Importer les routeurs
-// const contactRouter = require('./routes/contact.routes');
-// const categoryRouter = require('./routes/category.routes');
-// const userRouter = require('./routes/user.routes');
-// const authRouter = require('./routes/auth.routes');
-
-//! Importer la dépendance "serverless"
-import express, { Router } from "express";
-import serverless from "serverless-http";
+const contactRouter = require('../../routes/contact.routes');
+const categoryRouter = require('../../routes/category.routes');
+const userRouter = require('../../routes/user.routes');
+const authRouter = require('../../routes/auth.routes');
 
 //! Importer le middleware "checkJwt"
-const checkJwt = require('./middlewares/jwtCheck');
+const checkJwt = require('../../middlewares/jwtCheck');
+
+//! Importer Express
+const express = require('express');
+
+//! Importer "serverless-http"
+const serverless = require("serverless-http");
+
+//! Importer la dépendance "cors" et les options
+const cors = require('cors');
+const {generalCorsOptions, userCorsOptions} = require('../../cors/corsOptions');
+
+//! Importer la dépendance Dotenv pour gérer les variables d'environnement
+require('dotenv').config();
+
+//! Importer la dépendance http-status-codes pour gérer les codes HTTP
+const { StatusCodes } = require('http-status-codes');
 
 //! Créer l'app
 const app = express();
 
 //! Gérer les routes
-app.use('/contacts', cors(generalCorsOptions), checkJwt,'./routes/contact.routes');
-app.use('/categories', cors(generalCorsOptions), checkJwt, './routes/category.routes');
-app.use('/users', cors(generalCorsOptions), './routes/user.routes');
-app.use('/auth', cors(userCorsOptions), './routes/auth.routes');
+app.use('/contacts', cors(generalCorsOptions), checkJwt,contactRouter);
+app.use('/categories', cors(generalCorsOptions), checkJwt, categoryRouter);
+app.use('/users', cors(generalCorsOptions), userRouter);
+app.use('/auth', cors(userCorsOptions), authRouter);
 
-export const handler = serverless(api);
+// Export the app and the serverless function
+
+module.exports.handler = serverless(app);
+
